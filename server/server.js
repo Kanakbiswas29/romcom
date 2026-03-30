16cb4dcb1bfbb26057a4553bde1882a27d64a4eb
Kanakbiswas29 <kanakbiswas2945@gmail.com>
2026-03-30 22:37:42 +0530

    fix cors issue

--- server/server.js
+++ server/server.js
@@ -9,10 +9,16 @@ const watchlistRoutes = require('./routes/watchlist');
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: ["https://fsd-2-beige.vercel.app"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://fsd-2-beige.vercel.app"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 // Routes
