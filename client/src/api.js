86669710e87a38004644f72c1cb0ccc5c82ab20e
Kanakbiswas29 <kanakbiswas2945@gmail.com>
2026-04-02 13:14:06 +0530

    Update API baseURL to point to the new Render backend (romcom-xloe)

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://romcom-backend.onrender.com/api',
+  baseURL: 'https://romcom-xloe.onrender.com/api',
 });
 
 api.interceptors.request.use(
